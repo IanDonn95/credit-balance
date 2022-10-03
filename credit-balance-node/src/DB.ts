f25b6f4c289fb9f38ad5bb84970d451e7c50e5c0
Ian Donn <IanDonn95@gmail.com>
2022-10-03 17:41:46 -0400

    mongo queries

--- credit-balance-node/src/DB.ts
+++ credit-balance-node/src/DB.ts
@@ -1,5 +1,6 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { Collection, MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
+import { Creditor } from './Types';
 dotenv.config();
 
 
@@ -7,24 +8,34 @@ const MONGO_CREDENTIALS = process.env.MONGO_CREDENTIALS;
 const MONGO_URL = process.env.MONGO_URL;
 
 const client = new MongoClient(`${MONGO_URL}?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority`, {
-  sslKey: MONGO_CREDENTIALS,
-  sslCert: MONGO_CREDENTIALS,
-  serverApi: ServerApiVersion.v1
+	sslKey: MONGO_CREDENTIALS,
+	sslCert: MONGO_CREDENTIALS,
+	serverApi: ServerApiVersion.v1
 });
 
+async function connect() {
+	await client.connect();
+	const database = client.db("creditorsDB");
+	const collection = database.collection<Creditor>("creditors");
+	return collection;
+}
 
-async function run() {
-    try {
-      await client.connect();
-      const database = client.db("creditorsDB");
-      const collection = database.collection("creditors");
-      const docCount = await collection.countDocuments({});
-      console.log(docCount); // test
-      // perform actions using client
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
+async function close() {
+	try {
+		// await client.close(); // silently close when node exits instead?
+	} catch (e) {
+		console.error(e);
+	}
+}
 
-run().catch(console.dir);
+export async function query<D>(action: (collection: Collection<Creditor>) => Promise<D>) {
+	try {
+		const collection = await connect();
+		return await action(collection);
+	} catch (e) {
+		console.error(e);
+	}
+	 finally {
+		await close();
+	}
+}
--- credit-balance-node/src/index.ts
+++ credit-balance-node/src/index.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 
-import './DB';
+import { query } from './DB';
+import { Creditor } from './Types';
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 
-app.get('/getGoodCreditors', (req, res) => {
-    res.send([]);
+app.get('/getGoodCreditors', async (req, res) => {
+    const creditors = await query(col => col.find<Creditor>({
+        balance: { $gt: 2000 },
+        minPaymentPercentage: { $lte: 29.99 },
+    }).toArray());
+    res.send(creditors);
 });
 
-app.post(`/creditor`, (req, res) => {
-    res.send([req.body]);
+app.post(`/creditor`, async (req, res) => {
+    const inserted = await query(col => col.insertOne(req.body));
+    res.send([inserted]);
 })
 
 app.listen(port, () => {
