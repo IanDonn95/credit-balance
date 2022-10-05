dc0ee076aba68e3a0efa6c5bdc138a03f74b5f0e
Ian Donn <IanDonn95@gmail.com>
2022-10-04 21:21:53 -0400

    final azure changes

--- credit-balance-azure/creditor/index.ts
+++ credit-balance-azure/creditor/index.ts
@@ -2,6 +2,14 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { CREDITORS_ID_SEQUENCE_COLLECTION, CREDITORS_COLLECTION, getGoodCreditors } from "../lib/CreditorsCollection";
 import { query } from "../lib/DB";
 
+/**
+ * enable POST API call to add a new creditor
+ * first get the next unused ID to attach to the creditor
+ * this id is stored in a "sequence" collection that we increment each time we retrieve the next value
+ * then the creditor data is stored in the creditor collection, including the new ID
+ * DEV NOTE- a future improvement would be to convert this to a single DB transaction, but a partial save won't break the DB
+ * we would just skip an ID in the sequence
+ */
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request to add a creditor.');
     
@@ -12,8 +20,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     await query(CREDITORS_COLLECTION, col => col.insertOne(newCreditor)); // save the new creditor
     const creditors = await query(CREDITORS_COLLECTION, getGoodCreditors); // return up-to-date creditors list
 
+    context.log('added new creditor to Mongo');
+
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: creditors
     };
 
--- credit-balance-azure/goodCreditors/index.ts
+++ credit-balance-azure/goodCreditors/index.ts
@@ -2,24 +2,16 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { CREDITORS_COLLECTION, getGoodCreditors } from "../lib/CreditorsCollection";
 import { query } from '../lib/DB';
 
+// enable GET API call to find good creditors
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request for creditors.');
 
-    try {
-        const creditors = await query(CREDITORS_COLLECTION, getGoodCreditors);
-        context.log('retrieved creditors from Mongo');
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: JSON.stringify(creditors)
-        };
-    } catch {
-        context.log('failed to connect to Mongo');
-        context.res = {
-            body: 'failed to connect to Mongo'
-        }
-    }
+    const creditors = await query(CREDITORS_COLLECTION, getGoodCreditors);
+    context.log('retrieved creditors from Mongo');
 
-
+    context.res = {
+        body: creditors
+    };
 };
 
 export default httpTrigger;
